244f42a5bfaa7cd8b5350a75b20af1d9b4cf5e0e
munimrahman <munimrh@gmail.com>
2023-07-12 03:12:43 +0600

    public route

--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -1,1 +1,17 @@
-// hello
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PublicRoute = ({ children }) => {
+  const isLoggedIn = useAuth();
+  const auth = useSelector((state) => state.auth);
+  const role = auth?.user?.role;
+  const path = role === "admin" ? "/admin" : "/course/2";
+
+  return !isLoggedIn ? children : <Navigate to={path} />;
+};
+
+export default PublicRoute;
